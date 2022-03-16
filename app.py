baa6d78d7b60ca262112602480ecdca8fcd953c2
Naoki Sasaki <tejc180574@gmail.com>
2022-03-16 15:15:52 +0900

    create base

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def index():
+    return render_template('index.html')
+
+@app.route('/analytics', methods=['GET'])
+def analytics():
+    return render_template('analytics.html')
+    
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost')
